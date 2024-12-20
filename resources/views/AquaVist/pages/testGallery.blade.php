f6df0990653da7a6e128cd13fdf8272b8cc2c4f1
hashLahiru <hasinthalahiru333@gmail.com>
2024-12-20 11:56:36 +0530

    Bubble Basic Setup

--- resources/views/AquaVist/pages/testGallery.blade.php
+++ resources/views/AquaVist/pages/testGallery.blade.php
@@ -3,10 +3,7 @@
 @section('title', 'Home Page')
 
 @section('content')
-
-
-    <div id="page" class="hfeed site">
-
+    <div id="page" class="hfeed site info">
         <header id="masthead" itemscope="itemscope" itemtype="https://schema.org/WPHeader">
             <p class="main-title bhf-hidden" itemprop="headline"><a href="https://templatekit.jegtheme.com/aquavist"
                     title="Aquavist" rel="home">Aquavist</a></p>
